d4b7b84ec729051972ec58289651e5d7935a52e8
matt-tingen <matt.tingen@gmail.com>
2014-11-24 00:40:46 -0500

    Added buildscript

--- build.py
+++ build.py
@@ -1,9 +1,23 @@
 from zipfile import ZipFile
 import os
+import pytest
+import sys
 
 SRC_DIR = 'src'
+EXCLUDE = ['__pycache__', '__init__.py']
+PACKAGE_FILENAME = 'CommentReflow.sublime-package'
 
-with ZipFile('CommentReflow.sublime-package', 'w') as sublime_package:
-    for filename in os.listdir(SRC_DIR):
-        zip_name = os.path.join(SRC_DIR, filename)
-        sublime_package.write(zip_name, filename)
+test_result = pytest.main()
+
+if test_result == 0:
+    with ZipFile(PACKAGE_FILENAME, 'w') as sublime_package:
+        for filename in os.listdir(SRC_DIR):
+            if filename not in EXCLUDE:
+                zip_name = os.path.join(SRC_DIR, filename)
+                sublime_package.write(zip_name, filename)
+
+    print('\nPackage created: ' + PACKAGE_FILENAME)
+else:
+    print('\nTests failed. Build aborted.')
+
+sys.exit(test_result)
