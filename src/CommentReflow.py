10827757d1cbfc81524d9e457fa732f17496bc29
matt-tingen <matt.tingen@gmail.com>
2014-11-24 21:55:55 -0500

    Fixed issue when reflowing a line that touches a line boundary

--- src/CommentReflow.py
+++ src/CommentReflow.py
@@ -1,13 +1,6 @@
 import textwrap
 import re
 
-    # This is a comment with blank lines and nested indention. Lorem ipsum dolor sit
-    #
-    #     - amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.
-    #     - Maecenas congue ligula ac quam viverra nec consectetur ante
-    #     hendrerit.
-    # Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit
-    # amet vitae augue.
 
 class GreatestCommonPrefix:
     def __init__(self, regex=None, whitelist=None):
@@ -168,30 +161,23 @@ except:
 else:
     class CommentReflowCommand(sublime_plugin.TextCommand):
         def run(self, edit):
-            self.get_selection()
+            self.edit = edit
+            self.get_region()
+            selected_text = self.view.substr(self.region)
+
+            self.get_preferences()
+
+            reflow = ReflowComment(**self.preferences)
 
             try:
-                self.get_preferences()
-                reflow = ReflowComment(**self.preferences)
-                self.new_comment = reflow.reflow(self.selected_text)
+                self.new_comment = reflow.reflow(selected_text)
             except Exception:
                 sublime.status_message('Did not recognize selection as a comment')
             else:
-                self.replace_lines(edit)
+                self.replace_lines()
 
-        def replace_lines(self, edit):
-            classes = 0
-
-            if not self.view.classify(max(self.region.a, self.region.b)) & sublime.CLASS_LINE_END:
-                print('line end')
-                classes |= sublime.CLASS_LINE_END
-
-            if not self.view.classify(min(self.region.a, self.region.b)) & sublime.CLASS_LINE_START:
-                print('line start')
-                classes |= sublime.CLASS_LINE_START
-
-            region = self.view.expand_by_class(self.region, classes)
-            self.view.replace(edit, region, self.new_comment)
+        def replace_lines(self):
+            self.view.replace(self.edit, self.region, self.new_comment)
 
         def get_language_specific_settings(self, settings):
             scopes = self.view.scope_name(self.view.sel()[0].a).split()
@@ -207,7 +193,8 @@ else:
                            'semicolon': r';+'}
                 marker_regex = markers[comment_type]
             except (StopIteration, IndexError, KeyError):
-                # We are not in a comment.line.* scope or we are in one that is not accounted for.
+                # We are not in a comment.line.* scope or we are in one that is
+                # not accounted for.
                 self.marker = settings.get('comment_reflow_marker')
                 self.comment_start_regex = settings.get('comment_reflow_comment_start_regex')
 
@@ -222,10 +209,10 @@ else:
             settings = self.view.settings()
             self.get_language_specific_settings(settings)
 
-            # I would like to make the ruler the fall back for max_width but I have
-            # not been able to find a way to get the current ruler besides the
-            # setting 'rulers' which seems to be a global setting and doesn't refer
-            # to the current file's ruler.
+            # I would like to make the ruler the fall back for max_width but I
+            # have not been able to find a way to get the current ruler besides
+            # the setting 'rulers' which seems to be a global setting and
+            # doesn't refer to the current file's ruler.
             self.preferences = {
                 'marker': self.marker,
                 'max_width': int(settings.get('comment_reflow_width', 80)),
@@ -235,14 +222,13 @@ else:
                 'comment_start_regex': self.comment_start_regex,
                 'new_paragraph_regex': settings.get('comment_reflow_new_paragraph_regex', r'[*-] ')}
 
-        def get_selection(self):
-            # This assumes there is only one selection. We do not need to check for
-            # that condition because of the is_enabled method.
+        def get_region(self):
+            # This assumes there is only one selection. We do not need to check
+            # for that condition because of the is_enabled method.
             self.region = self.view.sel()[0]
-            # lines_touched is a list of regions
-            lines_touched = self.view.lines(self.region)
-            self.selected_text = '\n'.join(self.view.substr(region) for region in lines_touched)
-            return True
+
+            # Expand to the include the entire lines.
+            self.region = self.view.line(self.region)
 
         def is_enabled(self):
             return len(self.view.sel()) == 1
